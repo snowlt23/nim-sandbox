27dfaa6a8d4bdaefa0bded32f17892327af28689
snowlt23 <shsnow23@gmail.com>
2017-04-08 10:47:49 +0900

    update restrictcopy macro

--- src/restrict_copy_macro.nim
+++ src/restrict_copy_macro.nim
@@ -1,22 +1,27 @@
 
 import macros
-import boost.richstring
+import strutils, boost.richstring
 import options
+
+proc isViolationCopy*(valuenode: NimNode): bool =
+  if valuenode.kind == nnkEmpty: # var a: T
+    return false
+  elif getType(valuenode).kind == nnkBracketExpr and
+      ($getType(valuenode)[0] == "ref" or $getType(valuenode)[0] == "ptr"): # var a: ref T = t
+    return false
+  elif valuenode.kind == nnkObjConstr: # var a = T()
+    return false
+  elif valuenode.kind == nnkCall and ($valuenode[0]).startsWith("init"):
+    return false
+  else:
+    return true
 
 proc checkRestrictCopy*(node: NimNode): Option[NimNode] =
   if node.kind == nnkAsgn:
-    if node[1].kind != nnkObjConstr:
+    if node[1].isViolationCopy:
       return some(node)
   elif node.kind == nnkLetSection or node.kind == nnkVarSection:
-    let valuenode = node[0][2]
-    if valuenode.kind == nnkEmpty: # var a: T
-      discard
-    elif getType(valuenode).kind == nnkBracketExpr and
-        ($getType(valuenode)[0] == "ref" or $getType(valuenode)[0] == "ptr"): # var a: ref T = t
-      discard
-    elif valuenode.kind == nnkObjConstr: # var a = T()
-      discard
-    else:
+    if node[0][2].isViolationCopy:
       return some(node)
   elif node.kind in AtomicNodes:
     discard
@@ -44,14 +49,35 @@ type
 
 proc `$`(mi: PMyInt): string = $mi[]
 
+proc initMyInt*(): MyInt =
+  result.x = 1
+  result.y = 2
+proc copyMyInt*(mi: MyInt): MyInt = mi
+
+proc init*(mi: var MyInt) =
+  mi.x = 1
+  mi.y = 2
+
+proc copyReturn*(mi: MyInt): MyInt {.restrictcopy.} =
+  return mi # compile error
+proc copyResult*(mi: MyInt): MyInt {.restrictcopy.} =
+  result = mi # compile error
+
 proc main() {.restrictcopy.} =
   let a = MyInt(x: 1, y: 2)
-  let b = a # compile error
-  let c = PMyInt(x: 3, y: 4)
-  let d = c # works
+  # let acopy = a # compile error
+  let b = initMyInt()
+  # let bcopy = copyMyInt(b) # compile error
+  var c: MyInt
+  c.init()
+  let d = PMyInt(x: 3, y: 4)
+  let dcopy = d # works
   echo a
+  # echo acopy
   echo b
+  # echo bcopy
   echo c
   echo d
+  echo dcopy
 
 main()
